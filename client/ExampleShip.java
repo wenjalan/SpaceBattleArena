ba1991914ff15aa7ed60f290e3f45ef57311ca01
rasberrypai <rasberrypai@gmail.com>
2019-06-05 10:31:00 -0700

    basic find the center program

--- client/ExampleShip.java
+++ client/ExampleShip.java
@@ -5,6 +5,8 @@ import ihs.apcs.spacebattle.*;
 import ihs.apcs.spacebattle.commands.*;
 
 public class ExampleShip extends BasicSpaceship {
+    static ObjectStatus myShip;
+    Point center = new Point(670,365);
     public static void main(String[] args)
     {
 		String ip;
@@ -23,10 +25,29 @@ public class ExampleShip extends BasicSpaceship {
     {
         return new RegistrationData("Example Ship", new Color(255, 255, 255), 0);
     }
-    
     @Override
     public ShipCommand getNextCommand(BasicEnvironment env)
     {
-        return new IdleCommand(0.1);
+	myShip = env.getShipStatus();
+	if(myShip.getPosition().getDistanceTo(center) <= 75) return new BrakeCommand(0.0);
+	if(Math.abs(degToCenter()) > 5)
+	return new RotateCommand(degToCenter());
+	if(myShip.getSpeed() <= 1)
+	return new ThrustCommand('F', 5, 1.0);
+	return new IdleCommand(0.1);
     }
+    private int degToCenter(){
+	int destAngle = myShip.getPosition().getAngleTo(center)+180-(int)myShip.getOrientation();
+	if(destAngle > 180) destAngle -= 360;
+	return destAngle;
+    }
+
+    //further optimization below
+    private double timeToCenter(){
+	
+    }
+    private int brakeDistance(){
+	
+    }
+
 }
